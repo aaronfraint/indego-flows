8324964eefd69001d2a8252e02837ea633ae8eff
Aaron Fraint <aaronfraint@icloud.com>
2021-05-10 20:44:04 -0400

    initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+
+    entry: {
+        main: path.resolve(__dirname, "./src/index.js"),
+    },
+
+    output: {
+        path: path.resolve(__dirname, "./docs"),
+        filename: "[name].bundle.js",
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "testing 213123123123",
+            template: path.resolve(__dirname, "./src/template.html"),
+            filename: "index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin(),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+            },
+            // CSS, PostCSS, and Sass
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, "./docs"),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+};
